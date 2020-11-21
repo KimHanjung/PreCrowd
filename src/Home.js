2cbbc13fb6b0cf4715b5a4d23afa15d506e7dc79
chu <byebye671@gmail.com>
2020-11-22 02:29:37 +0900

    add admin detail menu

--- src/Home.js
+++ src/Home.js
@@ -1,25 +1,37 @@
-import React from "react";
-import { Link, Route, BrowserRouter as Router } from "react-router-dom";
-import './main.css'
+import React, { Component } from 'react';
+import { BrowserRouter, Link } from 'react-router-dom';
+import './main.css';
 
-function Home() {
+
+class Home extends Component {
+  render() {
   return (
-    <div className='home'>
-        <div className='wise'>
-            <pre>지식은 모일수록 가치있다.
-                <br/>-???-
-            </pre>
-        </div>
-        <div className='mainSelect'>
-        <Link to="/login">
-            <button className='mainbutton'>로그인</button>
-        </Link>
-        <Link to="/register">
-            <button className='mainbutton'>회원가입</button>
+      <body>
+        <header className='el-header'>
+        <div className='headercontents'>
+        <Link to="/">
+            <button className='title'>PRECROWD</button>
         </Link>
         </div>
-    </div>
+        </header>
+        <div className='home'>
+            <div className='wise'>
+                <pre>지식은 모일수록 가치있다.
+                    <br/>-???-
+                </pre>
+            </div>
+            <div className='mainSelect'>
+            <Link to="/login">
+                <button className='mainbutton'>로그인</button>
+            </Link>
+            <Link to="/register">
+                <button className='mainbutton'>회원가입</button>
+            </Link>
+            </div>
+        </div>
+    </body>
   );
+  }
 }
 
 export default Home;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Main from './main';
+import Paging from './paging';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Main />
+  <BrowserRouter>
+    <Paging/>
+  </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
