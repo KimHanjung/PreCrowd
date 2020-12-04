d9af0cbca125cda4cb27bcae31c56172169ae48f
michaelp3 <74169012+michaelp3@users.noreply.github.com>
2020-12-05 01:39:09 +0900

    평가자 구현 2020.12.05 01:39

--- src/scorer/monitor_score.js
+++ src/scorer/monitor_score.js
@@ -1,52 +1,82 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import Form from "react-validation/build/form";
+import Input from "react-validation/build/input";
+import CheckButton from "react-validation/build/button";
+import ReactFlexyTable from 'react-flexy-table';
+import 'react-flexy-table/dist/index.css';
+import { confirmAlert } from 'react-confirm-alert';
+import 'react-confirm-alert/src/react-confirm-alert.css';
 import axios from 'axios';
+import PopupScore from './popupscore';
+import { Route, Link } from "react-router-dom";
 
-const divStyle = {
-  color: 'white',
-  margin: 100,
-  font: 'Helvetica',
-}
 
-class Scorer extends Component {
-  state = {
-    ID : '',
-    data : [],
+const Managetask = (props) => {
+  const [data, setData] = useState([
+    {
+      id: 0,
+      Task_name: 'dafault',
+      Term: 0,
+      Desc: 'dafault',
+      Pass: 0,
+      Task_data_table_name: 'dafault',
+      Task_data_table_schema: 'dafault',
+    }
+  ]);
+
+  const [file_index, setFile_index] = useState(0);
+
+
+  const popUpScore = (fileindex) =>{
+    setFile_index(fileindex);
   }
 
-  getData = async () => {
-    const myjson = JSON.parse(localStorage.getItem("user"));
-    this.setState({ID: myjson.id});
-    let response = await axios.get('http://localhost:3001/src/api/todolist',{
-      params: {
-        id: this.state.ID
-      }
-    });
-    response = response.data;
-    this.setState({data: response});
-  };
 
-  componentDidMount(){
-    this.getData();
-  }
+  const columns=[
+    {
+      header: 'File name',
+      key: 'File name',
+      td: (data) => <div>{data.Parsing_file_name}</div>,
+    },
+    {
+      header: 'Score',
+      key: 'Score',
+      td: (data) => <div>{data.Score}</div>,
+    },
+    {
+      header: 'Pass',
+      key: 'Pass',
+      td: (data) => <div>{data.Pass}</div>,
+    },
+  ]
 
-  render() {
-    return (
-      <div style={divStyle}>
-        {
-          this.state.data.map((item) => {
-            return(
-              <div>
-              <p key = {item.key}>name: {item.Parsing_file_name}, file index: {item.File_index}</p>
-              </div>
+  useEffect(async () => {
+    // const myjson = JSON.parse(localStorage.getItem("user"));
+    // const URL = "URL" + myjson.id;
+    // let response = await axios.get(URL);
+    // response = response.data;
+    // setData(response);
+  },[]);
+
+
+  return (
+    <div className='white'>
+      <ReactFlexyTable title='Task Management' data={data} columns={columns}/>
+        <Route
+          path={`${props.match.url}/score_file`}
+          render={() => {
+            return (
+              <PopupScore
+                onClick={() => {
+                  props.history.push(props.match.url);
+                }}
+                File_index={file_index}
+              />
             );
-          })
-        }
-      </div>
-        
-    );
-  }
-}
+          }}
+        />
+    </div>
+  );
+};
 
-
-export default Scorer;
+export default Managetask;
--- src/scorer/score_file.js
+++ src/scorer/score_file.js
@@ -1,59 +1,74 @@
-import React, { Component } from 'react';
-import { getDownloadFile } from './file';
-import { saveAs } from 'file-saver'
+import React, { useState, useEffect } from "react";
+import Form from "react-validation/build/form";
+import Input from "react-validation/build/input";
+import CheckButton from "react-validation/build/button";
+import ReactFlexyTable from 'react-flexy-table';
+import 'react-flexy-table/dist/index.css';
+import { confirmAlert } from 'react-confirm-alert';
+import 'react-confirm-alert/src/react-confirm-alert.css';
+import axios from 'axios';
+import PopupScore from './popupscore';
+import { Route, Link } from "react-router-dom";
 
 
-class Scorer extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      ID:'',
-      score: 0,
-      pass: 0,
-      checked: false,
-    };
-}
-  downloadFile = () =>{
-    getDownloadFile()
-      .then(blob => saveAs(blob, 'file.csv'))
+const Managetask = (props) => {
+  const [data, setData] = useState([
+    {
+      id: 0,
+      Task_name: 'dafault',
+      Term: 0,
+      Desc: 'dafault',
+      Pass: 0,
+      Task_data_table_name: 'dafault',
+      Task_data_table_schema: 'dafault',
+    }
+  ]);
+
+  const [file_index, setFile_index] = useState(0);
+
+
+  const popUpScore = (fileindex) =>{
+    setFile_index(fileindex);
   }
 
-  changeHandler = event => {
-    const name = event.target.name;
-    const value = event.target.value;
 
-    this.setState({
-      [name]: value
-    });
-  }
+  const columns=[
+    {
+      header: 'File name',
+      key: 'File name',
+      td: (data) => <Link to={`${props.match.url}/score_file`}>
+        <button type = 'button' onClick = {() => popUpScore(data.File_index)}>{data.Parsing_file_name}</button>
+        </Link>,
+    },
+  ]
 
-  handleCheckbox = (e) => {
-    const { target: { checked } } = e;
-    this.setState({ checked });
-    console.log(this.state.checked);
+  useEffect(async () => {
+    const myjson = JSON.parse(localStorage.getItem("user"));
+    const URL = "http://localhost:3001/src/api/todolist?id=" + myjson.id;
+    let response = await axios.get(URL);
+    response = response.data;
+    setData(response);
+  },[]);
+
+
+  return (
+    <div className='white'>
+      <ReactFlexyTable title='Task Management' data={data} columns={columns}/>
+        <Route
+          path={`${props.match.url}/score_file`}
+          render={() => {
+            return (
+              <PopupScore
+                onClick={() => {
+                  props.history.push(props.match.url);
+                }}
+                File_index={file_index}
+              />
+            );
+          }}
+        />
+    </div>
+  );
 };
 
-  handleSubmit = () =>{ //submit 하면 서버로 점수와 패스 여부 보내줌
-
-  }
-
-  render() {
-    return (
-      <div style = {
-        {marginLeft: 100}
-      }>
-        <div>
-        <button className = 'scorebutton' type = 'button' onClick = {this.downloadFile}>Download</button>
-        </div>
-        <div>
-        <input style = {{width: 300}} type = 'text' onChange = {this.changeHandler} placeholder = "score"></input><br></br>
-        </div>
-        <div style = {{color: 'white'}}>Pass?
-        <input style = {{width: 30}} type = 'checkbox' checked = {this.state.checked} onChange = {this.handleCheckbox} placeholder = "pass/nonpass"></input>
-        </div>
-        <button type = 'button' onClick = {this.handleSubmit}>Submit</button>
-      </div>
-    );
-  }
-}
-export default Scorer;
+export default Managetask;
