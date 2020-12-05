77f0beb00d61fa2b9ffacb60404f2a2c27897fb9
SJ-moon <68596409+SJ-moon@users.noreply.github.com>
2020-12-06 03:03:52 +0900

    Update Participation.js

--- src/submitter/Participation.js
+++ src/submitter/Participation.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 import axios from 'axios';
 import { Link, withRouter } from 'react-router-dom';
 import { Alert } from 'bootstrap';
-import imgfile from './Signin.JPG';
+import imgfile from './SignIn.jpg';
 class Participation extends React.Component {
     constructor(props) {
         super(props);
